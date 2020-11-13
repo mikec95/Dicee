9d847185c40bfe96e07f9664cdfb28c6dc9a92ce
Mike Carbonari <mikecarbonari1@gmail.com>
2020-11-12 22:07:07 -0600

    Change IBOutlet variable names

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     // Create variables to reference UI elements
-    @IBOutlet weak var diceOne: UIImageView!
-    @IBOutlet weak var diceTwo: UIImageView!
+    @IBOutlet weak var diceLeft: UIImageView!
+    @IBOutlet weak var diceRight: UIImageView!
     @IBOutlet weak var buttonRoll: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Change image literals on load of view.
-        diceOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceTwo.image = #imageLiteral(resourceName: "DiceThree")
+
     }
 }
