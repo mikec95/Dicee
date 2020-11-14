ca19cee6bd29023459fef57033885ca5094eeba5
Mike Carbonari <mikecarbonari1@gmail.com>
2020-11-13 22:36:57 -0600

    Set random values for each dice on click of the roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
 
     // Set behavior of dice on press of roll button
     @IBAction func rollButtonPressed(_ sender: Any) {
-        
+        // Set random values for each dice
+        diceLeft.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0..<6)]
+        diceRight.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0..<6)]
     }
 
 }
